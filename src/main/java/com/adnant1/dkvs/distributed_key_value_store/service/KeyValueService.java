d4a87e5ace11db4cade1160954ad9c6b030b9467
adnant1 <adnantayib8@gmail.com>
2025-07-04 23:03:55 -0400

    feat: Implemented put service method

--- src/main/java/com/adnant1/dkvs/distributed_key_value_store/service/KeyValueService.java
+++ src/main/java/com/adnant1/dkvs/distributed_key_value_store/service/KeyValueService.java
@@ -1,5 +1,23 @@
 package com.adnant1.dkvs.distributed_key_value_store.service;
 
+import java.util.HashMap;
+import org.springframework.stereotype.Service;
+
+@Service
 public class KeyValueService {
+    private final HashMap<String, String> keyValueStore = new HashMap<>();
+
+    // Store or update a key-value pair
+    public void put(String key, String value) {
+        if (key == null || key.isEmpty()) {
+            throw new IllegalArgumentException("Key cannot be null or empty.");
+        }
+
+        if (value == null) {
+            throw new IllegalArgumentException("Value cannot be null.");
+        }
+
+        keyValueStore.put(key, value);
+    }
 
 }
