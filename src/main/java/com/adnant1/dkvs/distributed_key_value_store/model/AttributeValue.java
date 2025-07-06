7a4cfd12f0f9212050ca51d8a86693f2667d31f0
adnant1 <adnantayib8@gmail.com>
2025-07-05 20:40:27 -0400

    feat: Implemented a model class to represent each attribute with its type and value

---
+++ src/main/java/com/adnant1/dkvs/distributed_key_value_store/model/AttributeValue.java
@@ -0,0 +1,19 @@
+package com.adnant1.dkvs.distributed_key_value_store.model;
+
+public class AttributeValue {
+    private String S;
+
+    public AttributeValue() {}
+
+    public AttributeValue(String s) {
+        this.S = s;
+    }
+
+    public String getS() {
+        return S;
+    }
+
+    public void setS(String s) {
+        this.S = s;
+    }
+}
