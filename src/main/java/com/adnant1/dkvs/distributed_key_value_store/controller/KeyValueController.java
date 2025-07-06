2110b95fd260c8b29686ccaa45c84272b1ab2d44
adnant1 <adnantayib8@gmail.com>
2025-07-05 20:43:17 -0400

    refactor: Updated GET /dkvs/{key} controller method to use the new ItemResponse model

--- src/main/java/com/adnant1/dkvs/distributed_key_value_store/controller/KeyValueController.java
+++ src/main/java/com/adnant1/dkvs/distributed_key_value_store/controller/KeyValueController.java
@@ -1,5 +1,7 @@
 package com.adnant1.dkvs.distributed_key_value_store.controller;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.adnant1.dkvs.distributed_key_value_store.model.AttributeValue;
+import com.adnant1.dkvs.distributed_key_value_store.model.ItemResponse;
 import com.adnant1.dkvs.distributed_key_value_store.service.KeyValueService;
 
 @RestController
@@ -32,18 +37,25 @@ public class KeyValueController {
 
     // Retrieve a value by key
     @GetMapping("/dkvs/{key}")
-    public ResponseEntity<String> getKeyValue(@PathVariable String key) {
+    public ResponseEntity<ItemResponse> getKeyValue(@PathVariable String key) {
         try {
             String value = keyValueService.get(key);
+
+            Map<String, AttributeValue> itemMap = null;
             if (value != null) {
-                return ResponseEntity.ok(value);
-            } else {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Key not found.");
+                itemMap = Map.of(
+                    "key", new AttributeValue(key),
+                    "value", new AttributeValue(value)
+                );
             }
+
+            ItemResponse response = new ItemResponse(itemMap);
+            return ResponseEntity.ok(response);
+
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid input: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error retrieving value: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
