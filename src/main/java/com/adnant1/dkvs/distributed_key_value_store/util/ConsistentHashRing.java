5b7f907146c4b70e38d137e58851f04739ac6866
adnant1 <adnantayib8@gmail.com>
2025-07-07 20:55:31 -0400

    feat: Implemented private method hash using MD5

--- src/main/java/com/adnant1/dkvs/distributed_key_value_store/util/ConsistentHashRing.java
+++ src/main/java/com/adnant1/dkvs/distributed_key_value_store/util/ConsistentHashRing.java
@@ -1,5 +1,7 @@
 package com.adnant1.dkvs.distributed_key_value_store.util;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -13,7 +15,20 @@ public class ConsistentHashRing {
     }
 
     private int hash(String key) {
-       
+       try {
+           MessageDigest md = MessageDigest.getInstance("MD5");
+
+           byte[] digest = md.digest(key.getBytes(StandardCharsets.UTF_8));
+           int hash = ((digest[0] & 0xFF) << 24)
+                   | ((digest[1] & 0xFF) << 16)
+                   | ((digest[2] & 0xFF) << 8)
+                   | (digest[3] & 0xFF);
+
+           return hash & 0x7FFFFFFF;
+           
+       } catch (java.security.NoSuchAlgorithmException e) {
+           throw new RuntimeException("MD5 algorithm not found", e);
+       }
     }
 
     public void addNode(String nodeId) {
