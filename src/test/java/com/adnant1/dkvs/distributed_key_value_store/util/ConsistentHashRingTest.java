fb658786ad9551fe9720da507f12c1b646468a6e
adnant1 <adnantayib8@gmail.com>
2025-07-07 21:50:43 -0400

    feat: Structured test suite for ConsistentHashRing class

---
+++ src/test/java/com/adnant1/dkvs/distributed_key_value_store/util/ConsistentHashRingTest.java
@@ -0,0 +1,48 @@
+package com.adnant1.dkvs.distributed_key_value_store.util;
+
+import org.junit.jupiter.api.Test;
+
+public class ConsistentHashRingTest {
+    
+    private ConsistentHashRing ring = new ConsistentHashRing();
+
+    @Test
+    public void testAddSingleNode() {
+
+    }
+
+    @Test
+    public void testAddMultipleNodes() {
+    
+    }
+    
+    @Test
+    public void testRemoveNode() {
+    
+    }
+
+    @Test
+    public void testRemoveNonExistentNode() {
+
+    }
+
+    @Test
+    public void testAddNullNode() {
+
+    }
+
+    @Test
+    public void testGetNodeForKeyOnEmptyRing() {
+
+    }
+
+    @Test
+    public void testWrapAroundKey() {
+
+    }
+
+    @Test
+    public void testAddDuplicateNode() {
+
+    }
+}
