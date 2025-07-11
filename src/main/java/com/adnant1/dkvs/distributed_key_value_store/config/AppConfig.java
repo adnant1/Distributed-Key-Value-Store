a7ef72493db6c7149358b3a5abfd836cb69d6bd7
adnant1 <adnantayib8@gmail.com>
2025-07-11 00:08:17 -0400

    feat: Added a ConsistenHashRing bean for ring initialization on startup

--- src/main/java/com/adnant1/dkvs/distributed_key_value_store/config/AppConfig.java
+++ src/main/java/com/adnant1/dkvs/distributed_key_value_store/config/AppConfig.java
@@ -1,13 +1,22 @@
 package com.adnant1.dkvs.distributed_key_value_store.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import com.adnant1.dkvs.distributed_key_value_store.util.ConsistentHashRing;
 
 @Configuration
 public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public ConsistentHashRing consistentHashRing(@Value("${NODE_IDS:defaultNode1, defaultNode2}") String nodeIds) {
+        String[] initialNodes = nodeIds.split(",");
+        return new ConsistentHashRing(initialNodes);
     }
 }
